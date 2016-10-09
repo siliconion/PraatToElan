2d483ad1fd283ff79714f7dead52907ae29c2a5b
siliconion <chihchiayang@gmail.com>
2016-10-08 22:51:51 -0500

    save before change html file input to electron dialog

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,51 @@
-const fs = require('fs');
-const {dialog} = require('electron').remote;
-console.log(dialog);
+// const fs = require('fs');
+// const {dialog} = require('electron').remote;
+let inputFiles;
+let outputFolder = '$HOME/Desktop/PrattToElan/';
+let inputFilesDiv = document.getElementById("importFile");
 
-function getImportDir() {
-  alert(`get import {$importDir}`);
+function onImportFileChange() {
+  inputFiles = document.getElementById("importFile").files;
+  for(let i = 0; i< inputFiles.length; i++){
+    var n = document.createElement("DIV");        // Create a <button> element
+    var t = document.createTextNode(inputFiles[i].name);       // Create a text node
+    n.appendChild(t);
+    document.getElementById("importFiles").appendChild(n);
+  }
+  // document.getElementById("importFile").files.forEach((file) => {
+  //   document.getElementById("importFiles").appendChild(file.name);
+  // })
+  return false;
 }
 
 function getExportDir() {
   alert("get export")
 }
-function processFile() {
-
+function processFile(file) {
+  console.log("xxxxxx", file.webkitRelativePath, file.name);
 }
 function processFiles() {
-  let inputFiles = document.getElementById("importFile").files;
-  inputFiles.forEach(file => processFile(file));
+  // inputFiles.forEach(file => processFile(file));
+  for(let i = 0; i< inputFiles.length; i++){
+    processFile(inputFiles[i]);
+  }
 }
 function generateCSV() {
-  alert("gen csv")
+  processFiles();
 }
 
-function generateEAF() {
-  alert("gen eaf")
+// function generateEAF() {
+//   alert("gen eaf")
+// }
+
+// FileList.prototype.forEach = (cb) => {
+//   console.log("xxxxxx", this)
+//   for(let i = 0; i < this.length; i++){
+//     console.log("xxxxxx", i);
+//     cb(this[i]);
+//   }
+// };
+
+window.onload = () => {
+  document.getElementById("exportDir").value=outputFolder;
 }
