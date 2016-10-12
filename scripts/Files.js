36ed89d56fc88e001632a9408e08a3ba878a8626
siliconion <chihchiayang@gmail.com>
2016-10-12 16:04:59 -0500

    refacor data to be module

--- scripts/Files.js
+++ scripts/Files.js
@@ -1,52 +1,22 @@
 const fs = require('fs');
 const readline = require('readline');
-const {dialog} = require('electron').remote;
-// const DATA = require('./data');
-let inputFiles;
-let outputFolder = '/Users/CCYang/Desktop/';
+
+let Data = {};
+let outputFolder = '$HOME/Desktop/PrattToElan/';
 let threshold = 58;
 let buffer = 0.35;
 let averageSize = 11;
 let averageSpan = 5;
 
-function renderInputFiles(files) {
-  // innerHTML is slower, see http://stackoverflow.com/questions/3955229
-  document.getElementById('inputFiles').innerHTML = '';
-  inputFiles = files;
-  inputFiles.forEach(file => {
-    var node = document.createElement("DIV");
-    var t = document.createTextNode(file); 
-    node.appendChild(t);
-    document.getElementById('inputFiles').appendChild(node);
-  })
-  
+Data.setOutputFolder = function(input){
+  outputFolder = input;
 }
 
-function importFiles() {
-  dialog.showOpenDialog({
-      filters:[{name: 'csv', extensions: ['csv']},],
-      properties: ['openFile','multiSelections']
-    }, 
-    renderInputFiles
-  );
-}
 
-function renderOutputDiv(path) {
-  outputFolder = path;
-  document.getElementById("exportDir").value = outputFolder;
-}
-
-function selectExportDir() {
-  dialog.showOpenDialog({
-      properties: ['openDirectory'],
-    },
-    renderOutputDiv
-  );
-}
-function processFile(file) {
+Data.processFile = function processFile(file) {
   console.log('process one file', file);
   const lineReader = readline.createInterface({
-    input: fs.createReadStream(file),
+    input: fs.createReadStream(file, {autoClose:true}),
   });
   let lineIndex = 0;
   let valCache = [0];
@@ -66,13 +36,6 @@ function processFile(file) {
       // generate output file with header
       fs.appendFileSync(file, "start, end\n")
     } else {
-      /*
-        average of n:
-          n < averageSpan: self
-          n >= averageSpan: average of n-span to n+span
-        To get average of n, need to be at n+span
-      */
-      // for n < averageSpan
       timeCache.push(+lineVal[0]);
       valCache.push(+lineVal[1]);
       if(lineIndex <= averageSize) {
@@ -92,18 +55,20 @@ function processFile(file) {
       }
       // for n >= averageSpan
       if(lineIndex >= averageSize) {
-        timeCache.shift();
+        
         average = valTotal/averageSize;
         valTotal = valTotal - valCache.shift() + +lineVal[1];
-        console.log(lineIndex, average, timeCache, valCache);
+        // console.log("X", lineIndex, average);
+
         if(!speech && average > threshold ) {
           // start of a speech
           speech = true;
           // check previous end
           currentStart = Math.max(0,timeCache[(averageSize-1)/2]-buffer);
+          console.log("start of speech", timeCache[(averageSize-1)/2], prevStart, prevEnd, currentStart, timeCache)
           if(currentStart>prevEnd){
             // not overlap, write the privous end and this start to result
-            console.log("no overlap, write to file!!!!", currentStart, prevEnd)
+            console.log("no overlap, write to file!!!!", prevStart, prevEnd)
             fs.appendFileSync(file, prevStart +","+ prevEnd +"\n");
             prevStart = currentStart;
           }
@@ -112,35 +77,18 @@ function processFile(file) {
           // end of a speech
           prevEnd = timeCache[(averageSize-1)/2] + buffer;
           speech = false;
+          console.log("end of speech", timeCache[(averageSize-1)/2]);
         }
+        timeCache.shift();
       }
     }
     lineIndex ++;
   })
-}
-function processFiles() {
-  console.log('process files', inputFiles)
-  inputFiles.forEach(processFile);
-}
-function generateCSV() {
-  processFiles();
+  lineReader.on('close', data => {
+    console.log("closing!", prevStart, prevEnd);
+  });
+  // console.log("Last write", prevStart, prevEnd)
+  // fs.appendFileSync(file, prevStart +","+ prevEnd +"\n");
 }
 
-function setThreshold(input) {
-  threshold = input;
-  console.log("threshold", threshold);
-}
-
-function setBuffer(input) {
-  let inputN = +input;
-  if(typeof(buffer)==='number' && inputN > 0) {
-    buffer = +input;
-    console.log("buffer", buffer);
-  }
-}
-
-window.onload = () => {
-  renderOutputDiv(outputFolder);
-  document.getElementById("threshold").value = threshold;
-  document.getElementById("buffer").value = buffer;
-}
+module.exports = Data;
